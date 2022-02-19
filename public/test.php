9eb88836b88df6833dcda412a31c51c70ec58b68
Michael <74507178+mschwab82@users.noreply.github.com>
2022-02-20 00:22:19 +0100

    JSON Erweitert

--- public/test.php
+++ public/test.php
@@ -1,27 +1,56 @@
 <?php
+ 
+function dirToArray($dir) {
 
-$bild = 'images/gallery/test/20220213-149-00136.jpg';
+    $result = array();
+    $cdir = scandir($dir);
+    $imagepath = $dir;
 
-   if (exif_read_data($bild, 'IFD0')) {
-    $exif = exif_read_data($bild, 0, true);
-    echo $exif['COMPUTED']['Height'].'</br>';
-    echo $exif['COMPUTED']['Width'].'</br>';
-    echo $exif['COMPUTED']['ApertureFNumber'].'</br>';
-    echo $exif['IFD0']['Make'].'</br>';
-    echo $exif['IFD0']['Model'].'</br>';
-    echo $exif['IFD0']['Artist'].'</br>';
-    echo $exif['EXIF']['ExposureTime'].'</br>';
-    echo $exif['EXIF']['FNumber'].'</br>';
-    echo $exif['EXIF']['ISOSpeedRatings'].'</br>';
-    echo $exif['EXIF']['DateTimeOriginal'].'</br>';
-    echo $exif['EXIF']['ShutterSpeedValue'].'</br>';
-    echo $exif['EXIF']['ApertureValue'].'</br>';
-    echo $exif['EXIF']['UndefinedTag:0xA434'].'</br>';
-   }
+    foreach ($cdir as $key => $value) {
+        if (!in_array($value,array(".","..","01_JSON"))) {
 
-   // Additionnal informations from Lightroom
-   getimagesize($bild, $infos);
-   if ( isset($infos['APP13']) ) {
-       $tags = iptcparse($infos['APP13']);
-       print_r($tags['2#025']);
-   }
+            if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+                $filename = $imagepath.'01_JSON'.'/'.$value.'.json';
+
+                $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+                
+                $json_encoded = json_encode($result[$value]);
+                file_put_contents($filename, $json_encoded);
+            }
+            else {
+                $value_image = $imagepath.'/'.$value;
+                getimagesize($value_image, $infos);
+                $exif = exif_read_data($value_image, 0, true);
+                $tags = iptcparse($infos['APP13']);
+            
+                $metadata = array(
+                    $exif['FILE']['FileName'],
+                    $exif['COMPUTED']['Height'], 
+                    $exif['COMPUTED']['Width'], 
+                    $exif['COMPUTED']['ApertureFNumber'],
+                    $exif['IFD0']['Make'],
+                    $exif['IFD0']['Model'],
+                    $exif['IFD0']['Artist'],
+                    $exif['EXIF']['ExposureTime'],
+                    $exif['EXIF']['FNumber'],
+                    $exif['EXIF']['ISOSpeedRatings'],
+                    $exif['EXIF']['DateTimeOriginal'],
+                    $exif['EXIF']['ShutterSpeedValue'],
+                    $exif['EXIF']['ApertureValue'],
+                    $exif['EXIF']['UndefinedTag:0xA434'],
+                    $tags['2#025']
+                );
+            
+                $result[] = $metadata;
+            }
+        }
+    }
+
+    return $result;
+}
+
+$imagepath = 'images/gallery/';
+
+$items = dirToArray($imagepath);
+
+?>
