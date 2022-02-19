f1375db735d5a30fad56e352e24cb8a6d7725d6e
Michael <74507178+mschwab82@users.noreply.github.com>
2022-02-19 13:36:43 +0100

    Metadaten + Tags (Test)

--- public/test.php
+++ public/test.php
@@ -1,45 +1,27 @@
 <?php
 
-$bild = 'images/gallery/20220115-145-00029.jpg';
+$bild = 'images/gallery/test/20220213-149-00136.jpg';
 
-if (exif_read_data($bild, 'IFD0')) {
+   if (exif_read_data($bild, 'IFD0')) {
     $exif = exif_read_data($bild, 0, true);
-    foreach ($exif as $key => $section) {
-     foreach ($section as $name => $val) {
-    echo $selection['COMPUTED'].['Height'];
-    echo $exif['COMPUTED'].['Width'];
-    echo $exif['COMPUTED'].['ApertureFNumber'];
-    echo $exif['IFD0'].['Make'];
-    echo $exif['IFD0'].['Model'];
-    echo $exif['IFD0'].['Artist'];
-    echo $exif['EXIF'].['ExposureTime'];
-    echo $exif['EXIF'].['FNumber'];
-    echo $exif['EXIF'].['ISOSpeedRatings'];
-    echo $exif['EXIF'].['DateTimeOriginal'];
-    echo $exif['EXIF'].['ShutterSpeedValue'];
-    echo $exif['EXIF'].['ApertureValue'];
-    echo $exif['EXIF'].['UndefinedTag'];
-     }
-    }
+    echo $exif['COMPUTED']['Height'].'</br>';
+    echo $exif['COMPUTED']['Width'].'</br>';
+    echo $exif['COMPUTED']['ApertureFNumber'].'</br>';
+    echo $exif['IFD0']['Make'].'</br>';
+    echo $exif['IFD0']['Model'].'</br>';
+    echo $exif['IFD0']['Artist'].'</br>';
+    echo $exif['EXIF']['ExposureTime'].'</br>';
+    echo $exif['EXIF']['FNumber'].'</br>';
+    echo $exif['EXIF']['ISOSpeedRatings'].'</br>';
+    echo $exif['EXIF']['DateTimeOriginal'].'</br>';
+    echo $exif['EXIF']['ShutterSpeedValue'].'</br>';
+    echo $exif['EXIF']['ApertureValue'].'</br>';
+    echo $exif['EXIF']['UndefinedTag:0xA434'].'</br>';
    }
-/* 
-if (exif_read_data($bild, 'IFD0')) {
-    $exif = exif_read_data($bild);
-    echo $exif['FILE']['FileName'];
 
-
-echo $exif['FILE'].['FileName'];
-    echo $exif['COMPUTED'].['Height'];
-    echo $exif['COMPUTED'].['Width'];
-    echo $exif['COMPUTED'].['ApertureFNumber'];
-    echo $exif['IFD0'].['Make'];
-cho $exi    echo $exif['IFD0'].['Model'];
-    echo $exif['IFD0'].['Artist'];
-    echo $exif['EXIF'].['ExposureTime'];
-    echo $exif['EXIF'].['FNumber'];
-    ef['EXIF'].['ISOSpeedRatings'];
-    echo $exif['EXIF'].['DateTimeOriginal'];
-    echo $exif['EXIF'].['ShutterSpeedValue'];
-    echo $exif['EXIF'].['ApertureValue'];
-    echo $exif['EXIF'].['UndefinedTag'];
-} */
+   // Additionnal informations from Lightroom
+   getimagesize($bild, $infos);
+   if ( isset($infos['APP13']) ) {
+       $tags = iptcparse($infos['APP13']);
+       print_r($tags['2#025']);
+   }
