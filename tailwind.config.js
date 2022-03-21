dfa4cf22bf826610c7b980390204f85d04bf4b5c
Michael <74507178+mschwab82@users.noreply.github.com>
2022-03-21 23:31:45 +0100

    Tailwind-Pfadanpassung

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './templates/**/*.{twig,html,js}',
-    './public/01_Tests/*.html'
+    './public/**/*.html'
   ],
     future: {
       // removeDeprecatedGapUtilities: true,
