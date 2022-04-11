051d460f609c8fcd13f40007229a61720de803e6
Michael <74507178+mschwab82@users.noreply.github.com>
2022-04-11 23:35:41 +0200

    JS - Photoswipe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 import "../scss/style.scss";
 
-// import "./lightgallery.js";
+  // Include Lightbox 
+import PhotoSwipeLightbox from '../../node_modules/photoswipe/dist/photoswipe-lightbox.esm.js';
+import ObjectPosition from '../../node_modules/@vovayatsyuk/photoswipe-object-position/photoswipe-object-position.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  // may select multiple "galleries"
+  gallery: '#gallery--cropped-thumbs',
+
+  // Elements within gallery (slides)
+  children: 'a',
+
+  // setup PhotoSwipe Core dynamic import
+  pswpModule: () => import('../../node_modules/photoswipe/dist/photoswipe.esm.js')
+});
+
+new ObjectPosition(lightbox);
+
+lightbox.init();
