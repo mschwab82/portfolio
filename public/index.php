3a202cec1ac664eb27781668c77ce7485b7b585b
Michael <74507178+mschwab82@users.noreply.github.com>
2022-04-16 19:28:58 +0200

    Change Layout

--- public/index.php
+++ public/index.php
@@ -3,11 +3,6 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-// require_once './functions/Utilities.php';
-
-// require_once './functions/JSON_Merge.php';
-
-/* logData('Session wurde gestartet ' . $_SERVER['DOCUMENT_ROOT'] ); */
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
